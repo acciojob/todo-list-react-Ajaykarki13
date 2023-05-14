308d4c69770cc149bbf2fc749d58a4d84239bd6b
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-05-14 17:48:11 +0000

    chk

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,66 +1,76 @@
 
 import { indexOf } from "cypress/types/lodash";
 import React, {useState} from "react";
-
 const Todo = () => {
 
-    const[text,setText] = useState('');
-    const[list,setList] = useState([]) ;
-
-function handleAdd(e){
-setList([...list,text])
-setText('')
-}
-
-function handleSave(ind)
-{
-
-    const savetask = [...list]
-    savetask[ind] = text
-    setList(savetask)
-    setText('')
+    onst [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+    const [editIndex, setEditIndex] = useState(-1);
+    const [editTask, setEditTask] = useState("");
+  
+    const addTask = () => {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    };
+  
+    const deleteTask = (index) => {
+      const newTasks = [...tasks];
+      newTasks.splice(index, 1);
+      setTasks(newTasks);
+    };
+  
+    const startEditTask = (index, task) => {
+      setEditIndex(index);
+      setEditTask(task);
+    };
+  
     
-}
-
-function handleDelete(ind)
-{
-    const newList = [...list]
-    //setList(newList.filter((t,i)=>{i!==ind}))
-    newList.splice(ind,1)
-setList(newList);
-}
-
-
-  return (
-    <>
-    
-    <h2>To do list</h2>
-
-    <div className="add_tasks_section">
-
-    <input type = 'text' value={text} onChange={(e)=>setText(e.target.value)}  />
-    <button  className="add_button" onClick={handleAdd} >
-        Add</button>
-</div>
-
-<div className="tasks_section">
-
-    {list.map((t,i)=>(
-        <>
-            {t}
-            <button onClick= {()=>setText(task)}> Edit </button>
-<button onClick={()=>handleDelete(i)}> Delete </button>
-
-{text && i === list.indexOf(text) &&
-(<button  className="save" onClick={()=>handleSave(i)}> Save </button>)}
-
-</>
-
-    
-     ))}
-    </div>
-    </>
-  )
+    const saveEditTask = (index) => {
+      
+      const newTasks = [...tasks];
+      newTasks[index] = editTask;
+      setTasks(newTasks);
+      setEditIndex(-1);
+      setEditTask("");
+    };
+  
+    return (
+      <div>
+        <h1>To-Do App</h1>
+        <div className = 'add_tasks_section'>
+          <input
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+          <button onClick={addTask}>Add Task</button>
+        </div>
+        
+        <div className='tasks_section'>
+        
+          {tasks.map((task, index) => (
+            <div>
+              {editIndex === index ? (
+                <div className='save'>
+                  <input
+                    type="text"
+                    value={editTask}
+                    onChange={(e) => setEditTask(e.target.value)}
+                  />
+                  <button onClick={() => saveEditTask(index)}>Save</button>
+                </div>
+              ) : (
+                <div className='edit'>
+                  {task}
+                  <button onClick={() => startEditTask(index, task)}>Edit</button>
+                  <button onClick={() => deleteTask(index)}>Delete</button>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
 }
 
 export default Todo
